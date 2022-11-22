025d2f1a56c0bc8190c7ab0921769cb5e942e1ce
EdgarVRP <edgarvrp22@gmail.com>
2022-11-22 13:34:35 -0600

    Se añade el catch error

--- src/IO/IOTest6.java
+++ src/IO/IOTest6.java
@@ -4,14 +4,15 @@ import java.io.FileInputStream;
 
 public class IOTest6 {
     public static void main(String[] args) {
-        try{
-            FileInputStream fis =new FileInputStream("C:\\magia.txt");
-            int i=0;
-            while ((i= fis.read())!=-1){
+        try {
+            FileInputStream fis = new FileInputStream("C:\\magia.txt");
+            int i = 0;
+            while ((i = fis.read()) != -1) {
                 System.out.println((char) i);
             }
             fis.close();
-        }catch(){
+        } catch (Exception e) {
+            System.out.println(e);
 
         }
     }
